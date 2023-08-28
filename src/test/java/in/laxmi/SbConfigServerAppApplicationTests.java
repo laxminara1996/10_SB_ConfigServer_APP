276a6792bf4b234b7d6b21f6589122f344b861a2
laxminara1996 <laxminara1996@gmail.com>
2023-08-28 19:39:27 +0530

    initial commit

---
+++ src/test/java/in/laxmi/SbConfigServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.laxmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbConfigServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
